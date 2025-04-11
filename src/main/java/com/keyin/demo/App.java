2e72af5045ac69f92aa1958212a46277bc2db036
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2025-04-11 17:45:14 -0230

    Added App.java and tested JSON serialization.

---
+++ src/main/java/com/keyin/demo/App.java
@@ -0,0 +1,23 @@
+package com.keyin.demo;
+
+import com.keyin.demo.binarySearchTree.BinarySearchTree;
+
+public class App {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+
+        // Insert values
+        int[] numbers = {5, 2, 7, 1};
+        for (int num : numbers) {
+            bst.insert(num);
+        }
+
+        // Test traversal
+        System.out.println("In-order traversal: " + bst.inorderTraversal());
+
+        // Test JSON serialization
+        System.out.println("Tree structure (JSON):\n" + bst.toJson());
+
+
+    }
+}
