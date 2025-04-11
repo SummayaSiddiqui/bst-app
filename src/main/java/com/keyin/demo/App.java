ef537101befb28c2cf2c98a8b6f763d3b7921114
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2025-04-11 21:24:48 -0230

    Added initial UI setup with responsive layout.

--- src/main/java/com/keyin/demo/App.java
+++ src/main/java/com/keyin/demo/App.java
@@ -1,25 +1,11 @@
 package com.keyin.demo;
 
-import com.keyin.demo.binarySearchTree.BinarySearchTree;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        BinarySearchTree bst = new BinarySearchTree();
-
-        // Insert values
-        int[] numbers = {5, 2, 7, 1};
-        for (int num : numbers) {
-            bst.insert(num);
-        }
-
-        // Test traversal
-        System.out.println("In-order traversal: " + bst.inorderTraversal());
-
-        // Test JSON serialization
-        System.out.println("Tree structure (JSON):\n" + bst.toJson());
-
-        // Test balancing (bonus)
-        bst.balance();
-        System.out.println("Balanced tree (JSON):\n" + bst.toJson());
+        SpringApplication.run(App.class, args);
     }
 }
