fd58e8eed418b1aeceb0d605142b809e2d8bfbe4
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2025-04-11 22:04:54 -0230

    Added route(enter-numbers).

---
+++ src/main/java/com/keyin/demo/binarySearchTree/ViewController.java
@@ -0,0 +1,13 @@
+package com.keyin.demo.binarySearchTree;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/enter-numbers")
+    public String showInputPage() {
+        return "forward:/index.html";  // Serve the static file
+    }
+}
