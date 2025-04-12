3dad36e4fa3d40d93fa4749698fbcf2b9e492934
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2025-04-12 10:25:56 -0230

    Added processing-route.

--- src/main/java/com/keyin/demo/binarySearchTree/TreeRecordController.java
+++ src/main/java/com/keyin/demo/binarySearchTree/TreeRecordController.java
@@ -3,6 +3,9 @@ package com.keyin.demo.binarySearchTree;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/tree")
@@ -10,4 +13,22 @@ public class TreeRecordController {
 
     @Autowired
     private TreeRecordRepository treeRecordRepository;
+
+    @PostMapping("/process-numbers")
+    @ResponseBody
+    public Map<String, Object> processNumbersJson(@RequestBody Map<String, List<Integer>> request) {
+        List<Integer> numbers = request.get("numbers");
+        BinarySearchTree binarySearchTreeJson = new BinarySearchTree();
+
+        for (Integer num : numbers) {
+            binarySearchTreeJson.insert(num);
+        }
+
+        TreeRecord treeRecord = new TreeRecord();
+        treeRecord.setInputNumbers(numbers.toString());
+        treeRecord.setTreeStructure(binarySearchTreeJson.toJson());
+        treeRecordRepository.save(treeRecord);
+
+        return binarySearchTreeJson.toJsonRec(binarySearchTreeJson.getRoot());
+    }
 }
